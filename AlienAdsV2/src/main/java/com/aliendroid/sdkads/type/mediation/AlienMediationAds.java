3d132762b8caeb07a04f411e8060166156419ba8
rysasahrial-99 <rysasahrial@gmail.com>
2022-10-12 07:50:23 +0700

    RC-01

---
+++ AlienAdsV2/src/main/java/com/aliendroid/sdkads/type/mediation/AlienMediationAds.java
@@ -0,0 +1,44 @@
+package com.aliendroid.sdkads.type.mediation;
+
+import android.app.Activity;
+import android.widget.RelativeLayout;
+
+
+public class AlienMediationAds {
+
+    public static void SmallBanner(Activity activity, RelativeLayout layAds, String PlacementID){
+    }
+
+    public static void MediumBanner(Activity activity, RelativeLayout layAds, String PlacementID){
+
+    }
+
+    public static void BoardBanner(Activity activity, RelativeLayout layAds, String PlacementID){
+    }
+
+    public static void ScraperBanner(Activity activity, RelativeLayout layAds, String PlacementID){
+    }
+
+    public static void LoadInterstitial (Activity activity, String PlacementID) {
+
+    }
+
+    public static void ShowInterstitial (Activity activity) {
+    }
+
+    public static void LoadRewarded (String PlacementID) {
+           }
+
+    public static void ShowReward() {
+
+
+    }
+
+    public static void SmallNatives( Activity activity, RelativeLayout content, String PlacementID) {
+    }
+
+    public static void MediumNatives( Activity activity, RelativeLayout content, String PlacementID) {
+
+    }
+
+}
